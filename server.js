1dc023ba1e240ee70e9d7ab3fe10bebdacac1232
Austin Do <austinndo@gmail.com>
2022-08-24 17:17:44 -0700

    added a space to server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const cors = require("cors")
+const cors = require('cors')
 
-const AppRouter = require("./routes/AppRouter")
+const AppRouter = require('./routes/AppRouter')
 
 const PORT = process.env.PORT || 3001
 
@@ -10,6 +10,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => res.json({ message: "Server Works" }))
-app.use("/api", AppRouter)
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
